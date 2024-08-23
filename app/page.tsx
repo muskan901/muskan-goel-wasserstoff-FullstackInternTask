abd57c7fcc0aa015bb089bb820a495b64f7fcd92
Muskan Goel <110465204+muskan901@users.noreply.github.com>
2024-08-23 19:35:29 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 "use client"
 import React, { useState } from 'react'
-// import Header from "@/components/Header"
-import MainBody from "@/Components/Main"
-// import Footer from "@/Components/Footer"
-// import Link from 'next/link'
+import Main from "@/Components/Main"
 
 function page() {
   return (
     <>
-    <MainBody/>
+    <Main/>
     </>
   )
 }
