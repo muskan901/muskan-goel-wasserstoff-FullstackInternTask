adbc889bb58c9cd0ed87661c53c8b6355e67f80a
Muskan Goel <110465204+muskan901@users.noreply.github.com>
2024-08-28 10:08:06 +0530

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,5 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { FaSearch } from "react-icons/fa";
-// import clear from "../public/clear.png"
-// import night from "../public/night.png"
-// import cloudy from "../public/cloudy.png"
-// import drizzle from "../public/drizzle.png"
-// import rain from "../public/rain.png"
-// import thunder from "../public/thunder.png"
-// import snow from "../public/snow.png"
 
 const Main = () => {
   const inputRef = useRef();
@@ -39,33 +32,26 @@ const Main = () => {
   };
 
   const search = async (city) => {
-    if (city === "") {
+    if (!city) {
       alert("Enter city");
       return;
     }
 
     try {
       const apiKey = process.env.NEXT_PUBLIC_APP_ID;
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-
-      console.log("Weather Data:", data);
-
-      const { lon, lat } = data.coord;
-      const icon = allIcons[data.weather[0].icon] || "/clear.png";
-
-      // Fetch 5-day forecast data
-      const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+      const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
       const forecastResponse = await fetch(forecastUrl);
       if (!forecastResponse.ok) {
         throw new Error(`HTTP error! status: ${forecastResponse.status}`);
       }
       const forecastData = await forecastResponse.json();
 
+      console.log("Weather Data:", forecastData);
+
+      const currentWeather = forecastData.list[0];
+      const icon = allIcons[currentWeather.weather[0].icon] || "/clear.png";
+
+      // 5-day forecast data
       const lastFiveDays = forecastData.list
         .filter((_, index) => index % 8 === 0)
         .slice(0, 5)
@@ -81,20 +67,20 @@ const Main = () => {
         }));
 
       setWeatherData({
-        humidity: data.main.humidity,
-        windSpeed: Math.floor(data.wind.speed),
-        temperature: Math.floor(data.main.temp),
-        high: Math.floor(data.main.temp_max),
-        low: Math.floor(data.main.temp_min),
-        location: data.name,
-        desc: data.weather[0].main,
+        humidity: currentWeather.main.humidity,
+        windSpeed: Math.floor(currentWeather.wind.speed),
+        temperature: Math.floor(currentWeather.main.temp),
+        high: Math.floor(currentWeather.main.temp_max),
+        low: Math.floor(currentWeather.main.temp_min),
+        location: forecastData.city.name,
+        desc: currentWeather.weather[0].main,
         icon: icon,
-        time: new Date(data.dt * 1000).toLocaleTimeString([], {
+        time: new Date().toLocaleTimeString([], {
           hour: "2-digit",
           minute: "2-digit",
           hour12: true,
         }),
-        date: new Date(data.dt * 1000).toLocaleDateString(undefined, {
+        date: new Date(currentWeather.dt * 1000).toLocaleDateString(undefined, {
           day: "numeric",
           month: "long",
           year: "numeric",
@@ -107,12 +93,12 @@ const Main = () => {
     }
   };
 
-  useEffect(() => {
-    search("");
-  }, []);
+  // useEffect(() => {
+  //   search("");
+  // }, []);
 
   return (
-    <div className="bg-gradient-to-r from-customDark to-customLight h-screen">
+    <div className="bg-gradient-to-r from-customDark to-customLight min-h-screen">
       <div className="relative w-full flex justify-center items-center">
         <input
           ref={inputRef}
@@ -135,7 +121,7 @@ const Main = () => {
       <div className="flex justify-center items-center mt-11 ">
         <div className="w-96 h-52 p-4 bg-black bg-opacity-55 rounded-2xl text-white text-center">
           <h2 className="text-3xl font-semibold mt-6">
-            {weatherData.location || "Loading..."}
+            {weatherData.location || "Search for city"}
           </h2>
           <p className="text-4xl mt-6">{weatherData.time || ""}</p>
           <p className="text-lg">{weatherData.date || ""}</p>
@@ -145,7 +131,8 @@ const Main = () => {
           <>
             <div className="w-[450px] h-52 ml-9 p-2 bg-black bg-opacity-55 rounded-2xl text-white text-left">
               <h2 className="text-3xl font-semibold ml-2">
-              {convertTemperature(weatherData.temperature)}°{isCelsius ? "C" : "F"}
+                {convertTemperature(weatherData.temperature)}°
+                {isCelsius ? "C" : "F"}
               </h2>
               <div className="flex">
                 <img
@@ -153,7 +140,11 @@ const Main = () => {
                   alt="Weather icon"
                   className="w-12 h-12 mt-4"
                 />
-                <p className="mt-4"> Max: {convertTemperature(weatherData.high)}°{isCelsius ? "C" : "F"}</p>
+                <p className="mt-4">
+                  {" "}
+                  Max: {convertTemperature(weatherData.high)}°
+                  {isCelsius ? "C" : "F"}
+                </p>
                 <img
                   src={weatherData.icon}
                   alt="Weather icon"
@@ -168,7 +159,10 @@ const Main = () => {
               </div>
               <div className="flex">
                 <img src="low.png" alt="Weather icon" className="w-12 h-12 " />
-                <p className="mt-0">Min: {convertTemperature(weatherData.low)}°{isCelsius ? "C" : "F"}</p>
+                <p className="mt-0">
+                  Min: {convertTemperature(weatherData.low)}°
+                  {isCelsius ? "C" : "F"}
+                </p>
                 <h3 className="text-center mt-2 ml-16 text-2xl">
                   {weatherData.desc}
                 </h3>
@@ -187,23 +181,24 @@ const Main = () => {
       </div>
 
       <div className="flex justify-center items-center">
-        <div className="w-7/12 mt-8 h-auto p-4 bg-black bg-opacity-55 rounded-2xl text-white text-center">
+        <div className="w-7/12 mt-8 mb-4 h-auto p-4 bg-black bg-opacity-55 rounded-2xl text-white text-center">
           <h2 className="text-3xl font-semibold">5 Days Weather :</h2>
 
           {weatherData.lastFiveDays &&
             weatherData.lastFiveDays.map((day, index) => (
               <div
                 key={index}
-                className="flex justify-between items-center mt-2"
+                className="flex justify-between items-center mt-2 px-6"
               >
-                <img
-                  src={day.icon}
-                  alt="Weather icon"
-                  className="w-10 h-10 ml-10 "
-                />
-                <h3 className="text-center mt-2 text-2xl ">{day.desc}</h3>
-                <h2 className="text-xl font-semibold"> {convertTemperature(day.temperature)}°{isCelsius ? "C" : "F"}</h2>
-                <p className="text-lg mr-10">{day.date}</p>
+                <img src={day.icon} alt="Weather icon" className="w-16 h-14 " />
+                <h3 className="text-center mt-2 flex-1 text-2xl ">
+                  {day.desc}
+                </h3>
+                <h2 className="text-xl text-center flex-1 font-semibold">
+                  {" "}
+                  {convertTemperature(day.temperature)}°{isCelsius ? "C" : "F"}
+                </h2>
+                <p className="text-lg flex-1">{day.date}</p>
               </div>
             ))}
         </div>
